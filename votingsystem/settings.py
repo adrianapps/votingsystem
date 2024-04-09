b9210616d923e3af6dd8d9b64739bf5272e38087
Adam Akimowicz <adam.akimowicz13@onet.eu>
2024-04-09 11:08:18 +0200

    zmiana

--- votingsystem/settings.py
+++ votingsystem/settings.py
@@ -55,11 +55,7 @@ ROOT_URLCONF = 'votingsystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
-=======
         'DIRS': [os.path.join(BASE_DIR,"templates")],
->>>>>>> master
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,15 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-<<<<<<< HEAD
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-=======
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
->>>>>>> master
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
