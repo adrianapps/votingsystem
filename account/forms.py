a4c2d00ebe2f17c8a41c2132bbc02d022abec5f0
adrian <bulsar@tutanota.com>
2024-05-04 14:44:08 +0200

    fix registration form

--- account/forms.py
+++ account/forms.py
@@ -1,14 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import PasswordInput
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'E-mail'}))
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = PasswordInput(attrs={'placeholder': 'Password'})
+        self.fields['password2'].widget = PasswordInput(attrs={'placeholder': 'Confirm Password'})
 
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
