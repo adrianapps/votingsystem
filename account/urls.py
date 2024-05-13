c060006a73d6aa1af99155d008eb85dae1e67c3d
tempestpulse <bulsar@tutanota.com>
2024-05-13 23:56:17 +0200

    add urls for password reset

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from account.views import RegisterView
 from . import views
@@ -10,4 +11,20 @@ urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name="profile"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password_reset.html'
+    ),
+         name="password-reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password_reset_done.html'
+    ),
+         name="password-reset-done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password_reset_confirm.html'
+    ),
+         name="password-reset-confirm"),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password_reset_complete.html'
+    ),
+         name="password-reset-complete"),
 ]
--- votingsystem/urls.py
+++ votingsystem/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('election.urls')),
-    path('', include('account.urls'))
+    path('', include('account.urls')),
+    path('', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
