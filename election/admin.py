879771d8703fb37fe1f37e1bf9897b3cadce79dd
adrian <bulsar@tutanota.com>
2024-05-15 22:50:22 +0200

    configure admin for managing elections, voters and candidates

--- election/admin.py
+++ election/admin.py
@@ -2,8 +2,35 @@ from django.contrib import admin
 
 from .models import Election, Candidate, Party, Vote, Voter
 
-admin.site.register(Election)
-admin.site.register(Candidate)
+
+class CandidateInLine(admin.TabularInline):
+    model = Candidate
+    extra = 3
+
+
+class ElectionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["title", "description", "image", "max_candidates_choice"]}),
+        ("Date information", {"fields": ["start_date", "end_date"]}),
+    ]
+    inlines = [CandidateInLine]
+    list_display = ["title", "start_date", "end_date"]
+
+
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ["election", "user", "has_voted"]
+
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ["name", "election", "party"]
+    fieldsets = [
+        (None, {"fields": ["name", "party", "picture"]}),
+        ("Candidate's election", {"fields": ["election"]})
+    ]
+
+
+admin.site.register(Election, ElectionAdmin)
+admin.site.register(Candidate, CandidateAdmin)
 admin.site.register(Party)
 admin.site.register(Vote)
-admin.site.register(Voter)
+admin.site.register(Voter, VoterAdmin)
