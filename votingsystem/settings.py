a0d66e3abd303be4f590b937c441fc7e66cbf507
tempestpulse <bulsar@tutanota.com>
2024-05-28 18:36:13 +0200

    add static root

--- votingsystem/settings.py
+++ votingsystem/settings.py
@@ -146,6 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 
